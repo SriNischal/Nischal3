8cf0b205e55ca3ed6ced79f840bb9ff4e21a0fd4
SriNischal <srinischal.thiparani@atmecs.com>
2019-07-31 19:19:59 +0530

    Nischal

---
+++ Menu.java
@@ -0,0 +1,11 @@
+package Nischal;
+import java.util.Scanner;
+public class Menu {
+	public static void main(String[] args) 
+		{
+		Scanner scan=new Scanner(System.in);
+		Login login=new Login();
+		System.out.println("EMPLOYEE MANAGEMENT");
+		Login.login();
+		}
+		}
